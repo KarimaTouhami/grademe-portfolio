99608bc1c83052a711a0560fe8264a8c70f7394e
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2026-07-08 17:41:30 +0000

    Solve 2 exercises
    Validated on Grademe on 2026-07-08.
    
    - snake-fmt (practice)
    - seq-scan (practice)
    
    Every exercise here passed the Grademe grader: https://grademe.io
    
    Co-authored-by: Grademe <320286061+grademe-sync[bot]@users.noreply.github.com>

---
+++ c-core/snake-fmt/snake_fmt.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	char	lower;
+
+	if (argc != 2)
+	{
+		write(1, "wrong number of arguments\n", 26);
+		return (0);
+	}
+	
+	i = 0;
+	while (argv[1][i])
+	{
+		if (argv[1][i] >= 'A' && argv[1][i]<= 'Z')
+		{
+			lower = argv[1][i] + 32;
+			write(1, "_", 1);
+			write(1, &lower, 1);
+		}
+		else
+		{
+			write(1, &argv[1][i], 1);
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
