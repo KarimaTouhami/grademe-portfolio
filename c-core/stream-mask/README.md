2209b73e4ba38a4a514a932b068835cd6a5f33e2
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2026-09-17 12:52:18 +0000

    Solve stream-mask
    Validated on Grademe on 2026-09-17.
    
    - stream-mask (practice)
    
    Every exercise here passed the Grademe grader: https://grademe.io
    
    Co-authored-by: Grademe <320286061+grademe-sync[bot]@users.noreply.github.com>

--- c-core/stream-mask/README.md
+++ c-core/stream-mask/README.md
@@ -6,6 +6,6 @@ Read stdin and mask every non-overlapping occurrence of a given token
 **Allowed functions:** read, write, strlen, memmem, memmove, malloc, calloc, realloc, free, printf, fprintf, perror
 
 Solved in practice.
-Validated 2 times. Earlier versions are in this file's git history.
+Validated 3 times. Earlier versions are in this file's git history.
 
 [Read the full exercise on Grademe](https://grademe.io/app/exercise/stream-mask)
--- c-core/stream-mask/stream_mask.c
+++ c-core/stream-mask/stream_mask.c
@@ -1,77 +1,98 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
-static int	stream_match(const char *win, const char *token, size_t len)
+#define BUF_SIZE 8192
+
+static int	write_stars(size_t len)
 {
+	char	stars[256];
 	size_t	i;
+	size_t	chunk;
 
 	i = 0;
-	while (i < len)
+	while (i < 256)
+		stars[i++] = '*';
+	while (len > 0)
 	{
-		if (win[i] != token[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-static void	print_stars(size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-	{
-		write(1, "*", 1);
-		i++;
-	}
-}
-
-static int	process_stream(const char *token, size_t t_len)
-{
-	char	win[4096];
-	char	c;
-	size_t	w_len;
-	ssize_t	ret;
-
-	w_len = 0;
-	while (1)
-	{
-		ret = read(0, &c, 1);
-		if (ret < 0)
-			return (write(2, "Error\n", 6), 1);
-		if (ret == 0)
-			break ;
-		win[w_len++] = c;
-		if (w_len == t_len)
+		chunk = (len > 256) ? 256 : len;
+		if (write(1, stars, chunk) < 0)
 		{
-			if (stream_match(win, token, t_len))
+			perror("write");
+			return (-1);
+		}
+		len -= chunk;
+	}
+	return (0);
+}
+
+static int	process_stream(const char *pat, size_t pat_len)
+{
+	char	buf[BUF_SIZE];
+	size_t	buf_len;
+	ssize_t	bytes;
+	char	*match;
+	size_t	i;
+	size_t	safe_len;
+
+	buf_len = 0;
+	while ((bytes = read(0, buf + buf_len, BUF_SIZE - buf_len)) > 0)
+	{
+		buf_len += bytes;
+		i = 0;
+		while (i + pat_len <= buf_len)
+		{
+			match = memmem(buf + i, buf_len - i, pat, pat_len);
+			if (match)
 			{
-				print_stars(t_len);
-				w_len = 0;
+				if (match > buf + i && write(1, buf + i, match - (buf + i)) < 0)
+				{
+					perror("write");
+					return (1);
+				}
+				if (write_stars(pat_len) < 0)
+					return (1);
+				i = (match - buf) + pat_len;
 			}
 			else
 			{
-				write(1, &win[0], 1);
-				memmove(win, win + 1, --w_len);
+				safe_len = buf_len - i - (pat_len - 1);
+				if (safe_len > 0)
+				{
+					if (write(1, buf + i, safe_len) < 0)
+					{
+						perror("write");
+						return (1);
+					}
+					i += safe_len;
+				}
+				break ;
 			}
 		}
+		if (i > 0)
+		{
+			memmove(buf, buf + i, buf_len - i);
+			buf_len -= i;
+		}
 	}
-	if (w_len > 0)
-		write(1, win, w_len);
+	if (bytes < 0)
+	{
+		perror("read");
+		return (1);
+	}
+	if (buf_len > 0 && write(1, buf, buf_len) < 0)
+	{
+		perror("write");
+		return (1);
+	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	char	*token;
-	size_t	t_len;
-
-	if (argc != 2)
+	if (argc != 2 || !argv[1] || argv[1][0] == '\0')
 		return (1);
-	token = argv[1];
-	t_len = strlen(token);
-	if (t_len == 0 || t_len > 4096)
+	if (process_stream(argv[1], strlen(argv[1])) != 0)
 		return (1);
-	return (process_stream(token, t_len));
+	return (0);
 }
